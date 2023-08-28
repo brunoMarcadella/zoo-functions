7d50481648ab524664ee02e31750e36dd1052d97
Bruno Marcadella <bruno.marcadella@hotmail.com>
2023-08-28 17:18:59 -0300

    Requisito 1 realizado

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
-  // seu código aqui
+const getSpeciesByIds = (...ids) => {
+  const array = [];
+  const forEachId = ids.find((id) => {
+    const find = data.species.find((specie) => specie.id === id);
+    if (find !== undefined) {
+      array.push(find);
+    }
+  });
+
+  return array;
 };
+
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 module.exports = getSpeciesByIds;
