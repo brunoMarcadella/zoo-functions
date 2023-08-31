2d2cde0aec683f643b06ae9039ff3eb8ebd819db
Bruno Marcadella <bruno.marcadella@hotmail.com>
2023-08-31 20:45:54 -0300

    Fix: Requisito 3 problema do lint na linha 6

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,20 +1,22 @@
 const data = require('../data/zoo_data');
 
 const getEmployeeByName = (employeeName) => {
-  const emplData = data.employees;
+  const employeeData = data.employees;
 
-  const employeeInfos = emplData.find((employee) => {
+  const employeeInfos = employeeData.find((employee) => {
     const employeeFirstName = employee.firstName;
     const employeeLastName = employee.lastName;
 
     if (employeeFirstName === employeeName || employeeLastName === employeeName) {
       return employee;
     }
+    return undefined;
   });
 
   if (employeeInfos === undefined) {
     return {};
   }
+
   return employeeInfos;
 };
 console.log(getEmployeeByName('Nigel'));
