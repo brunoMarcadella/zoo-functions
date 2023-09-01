762e169f20939f1f86338a971067b15ca507f1ec
Bruno Marcadella <bruno.marcadella@hotmail.com>
2023-09-01 11:36:07 -0300

    getAnimalMap bloqueando a correção

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,56 +1,56 @@
 const data = require('../data/zoo_data');
 
-const findSpeciesWithLocation = (location) => {
-  const animals = [];
-  data.species.forEach((specie) => {
-    if (specie.location === location) {
-      animals.push(specie.name);
-    }
-  });
+// const findSpeciesWithLocation = (location) => {
+//   const animals = [];
+//   data.species.forEach((specie) => {
+//     if (specie.location === location) {
+//       animals.push(specie.name);
+//     }
+//   });
 
-  return animals;
-};
+//   return animals;
+// };
 
-const noParameter = () => ({
-  NE: findSpeciesWithLocation('NE'),
-  NW: findSpeciesWithLocation('NW'),
-  SE: findSpeciesWithLocation('SE'),
-  SW: findSpeciesWithLocation('SW'),
-});
+// const noParameter = () => ({
+//   NE: findSpeciesWithLocation('NE'),
+//   NW: findSpeciesWithLocation('NW'),
+//   SE: findSpeciesWithLocation('SE'),
+//   SW: findSpeciesWithLocation('SW'),
+// });
 
-const findSpeciesAndNamesWithLocation = (location) => {
-  const animals = [];
-  data.species.forEach((specie) => {
-    if (specie.location === location) {
-      const arrayOfNames = [];
-      specie.residents.forEach((resident) => {
-        arrayOfNames.push(resident.name);
-      })
-      animals.push({specie[name]: arrayOfNames});
-    }
-  });
+// const findSpeciesAndNamesWithLocation = (location) => {
+//   const animals = [];
+//   data.species.forEach((specie) => {
+//     if (specie.location === location) {
+//       const arrayOfNames = [];
+//       specie.residents.forEach((resident) => {
+//         arrayOfNames.push(resident.name);
+//       })
+//       animals.push({specie[name]: arrayOfNames});
+//     }
+//   });
 
-  return animals;
-};
+//   return animals;
+// };
 
-const speciesAndNames = () => {
-  return {
-    NE: findSpeciesAndNamesWithLocation('NE'),
-    NW: findSpeciesAndNamesWithLocation('NW'),
-    SE: findSpeciesAndNamesWithLocation('SE'),
-    SW: findSpeciesAndNamesWithLocation('SW'),
-  }
-}
+// const speciesAndNames = () => {
+//   return {
+//     NE: findSpeciesAndNamesWithLocation('NE'),
+//     NW: findSpeciesAndNamesWithLocation('NW'),
+//     SE: findSpeciesAndNamesWithLocation('SE'),
+//     SW: findSpeciesAndNamesWithLocation('SW'),
+//   }
+// }
 
-const getAnimalMap = (options) => {
-  if (options === undefined || !Object.keys(options).includes('includeNames')) {
-    return noParameter();
-  }
-  if (Object.keys(options).includes('includeNames') && Object.keys(options).length === 1) {
-    return speciesAndNames();
-  }
-  return 1;
-};
+// const getAnimalMap = (options) => {
+//   if (options === undefined || !Object.keys(options).includes('includeNames')) {
+//     return noParameter();
+//   }
+//   if (Object.keys(options).includes('includeNames') && Object.keys(options).length === 1) {
+//     return speciesAndNames();
+//   }
+//   return 1;
+// };
 
-console.log(getAnimalMap({ includeNames: true }));
+// console.log(getAnimalMap({ includeNames: true }));
 module.exports = getAnimalMap;
