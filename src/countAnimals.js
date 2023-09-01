892512609d1af763a1722f5804e84135f6fe5716
Bruno Marcadella <bruno.marcadella@hotmail.com>
2023-09-01 00:02:35 -0300

    Fix: requisito 5 lint problem

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,17 +2,17 @@ const data = require('../data/zoo_data');
 
 const countAnimals = (animal = 'all Animals') => {
   if (Object.keys(animal).length === 1) {
-    const speciePopular = data.species.find((specie) => specie.name === animal.species).residents.length;
-    return speciePopular;
+    const quantity = data.species.find((specie) => specie.name === animal.species).residents.length;
+    return quantity;
   } if (Object.keys(animal).length === 2) {
     const findSpecie = data.species.find((specie) => specie.name === animal.species);
-    const speciePopularity = findSpecie.residents.reduce((acc, resident) => {
+    const quantity = findSpecie.residents.reduce((acc, resident) => {
       if (resident.sex === animal.sex) {
         return acc + 1;
       }
       return acc;
     }, 0);
-    return speciePopularity;
+    return quantity;
   }
   const allAnimalsPopularity = {};
   data.species.forEach((specie) => {
