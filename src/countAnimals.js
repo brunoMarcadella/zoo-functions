12c2d499f3093857282bebfdab9401fb988c8775
Bruno Marcadella <bruno.marcadella@hotmail.com>
2023-08-31 23:58:47 -0300

    Requisito 5 finalizado

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,25 @@
 const data = require('../data/zoo_data');
 
-const countAnimals = (animal) => {
-  // seu código aqui
+const countAnimals = (animal = 'all Animals') => {
+  if (Object.keys(animal).length === 1) {
+    const speciePopular = data.species.find((specie) => specie.name === animal.species).residents.length;
+    return speciePopular;
+  } if (Object.keys(animal).length === 2) {
+    const findSpecie = data.species.find((specie) => specie.name === animal.species);
+    const speciePopularity = findSpecie.residents.reduce((acc, resident) => {
+      if (resident.sex === animal.sex) {
+        return acc + 1;
+      }
+      return acc;
+    }, 0);
+    return speciePopularity;
+  }
+  const allAnimalsPopularity = {};
+  data.species.forEach((specie) => {
+    allAnimalsPopularity[specie.name] = specie.residents.length;
+  });
+  return allAnimalsPopularity;
 };
 
+console.log(countAnimals());
 module.exports = countAnimals;
