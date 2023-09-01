849caee2a749d0058d7f3805b10aa85ed25e2a41
Bruno Marcadella <bruno.marcadella@hotmail.com>
2023-09-01 02:26:22 -0300

    Requisito 10 finalizado

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const { species } = data;
+  const personId = data.employees.find((employee) => employee.id === id);
+  let oldestAnimal = { age: 0 };
+  species.find((specie) => specie.id === personId.responsibleFor[0]).residents.forEach((resid) => {
+    if (resid.age > oldestAnimal.age) {
+      oldestAnimal = resid;
+    }
+  });
+
+  return Object.values(oldestAnimal);
 };
 
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 module.exports = getOldestFromFirstSpecies;
